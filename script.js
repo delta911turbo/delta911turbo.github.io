2c9fb52e0a17a7790c43386bc4ce85518ad3d963
Delta911Turbo <delta911turbo@users.noreply.github.com>
2024-02-12 01:01:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 
     loaderScript.onload = () => {
         // Load 3D model
-        const modelPath = './3dmodels/HeadHole.glb';
+        const modelPath = './3dmodels/HoleHead.glb';
 
         async function loadModel(path) {
             try {
